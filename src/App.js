00b5d55fc8711b6a591a565e9f15cbc45beb0aa2
Sbax <heysbax@protonmail.com>
2022-07-02 16:36:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { generateCharacter } from "./generator";
 
 function App() {
+  const character = generateCharacter();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      <h1>Generatore Personaggi per Cairn</h1>
+      <div className="character">
+        <h1>Aspetto</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sei <i>{character.name}</i>, in passato eri un {character.background}.{" "}
+          {character.traits} Hai {character.age} anni.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <h1>Attributi</h1>
+        <div>
+          <b>HP:</b> {character.hp}
+        </div>
+        <div>
+          <b>Armatura:</b> {character.armorValue}
+        </div>
+        <div>
+          <b>FOR:</b> {character.str}
+        </div>
+        <div>
+          <b>DES:</b> {character.dex}
+        </div>
+        <div>
+          <b>VOL:</b> {character.wil}
+        </div>
+
+        <h1>Equipaggiamento</h1>
+        <div>
+          <b>Armatura</b>: {character.armor}
+        </div>
+        <div>
+          <b>Arma</b>: {character.weapon}
+        </div>
+        <div>
+          <b>Attrezzi, strumenti e chincaglieria</b>:{" "}
+          {character.gearToolsAndTrinkets}
+        </div>
+        <div>
+          {(() => {
+            if (character.bonus.name) {
+              return (
+                <>
+                  <b>divbro di Incantesimi:</b> {character.bonus.name},{" "}
+                  {character.bonus.description.toLowerCase()}`
+                </>
+              );
+            }
+
+            return (
+              <>
+                <b>Bonus</b>: {character.bonus}
+              </>
+            );
+          })()}
+        </div>
+        <div>
+          <b>Attrezzatura di partenza</b>:{" "}
+          <ul>
+            {character.startingSupplies.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <p>
+        <a href="/">Ricarica la pagina</a> per generare un nuovo personaggio.
+      </p>
+      <p>
+        Basato sul generatore di personaggi per{" "}
+        <a href="https://cairnrpg.com/">Cairn</a> di{" "}
+        <a href="https://nakade.itch.io/">Nakade</a>.
+      </p>
+      <p>
+        Traduzioni a cura di{" "}
+        <a href="https://ita-translation-alliance.itch.io/cairn-ita">
+          Italian Translation Alliance
         </a>
-      </header>
-    </div>
+        .
+      </p>
+    </main>
   );
 }
 
---
+++ src/generator.js
@@ -0,0 +1,148 @@
+import generator from "./generator.it.json";
+
+const getRandomFromArray = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const getRandomBetween = (min = 1, max = 20) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomFromItemArray = (array) => {
+  const generated = getRandomBetween();
+
+  const sorted = array.sort((a, b) => {
+    if (a.value > b.value) return -1;
+    if (a.value < b.value) return 1;
+
+    return 0;
+  });
+
+  const toReturn = sorted.find(({ value }) => generated >= value);
+  return toReturn;
+};
+
+const getFirstName = (filter) => {
+  if (filter?.female) return getRandomFromArray(generator.firstNames.females);
+  if (filter?.male) return getRandomFromArray(generator.firstNames.males);
+
+  return getRandomFromArray([
+    ...generator.firstNames.females,
+    ...generator.firstNames.males,
+  ]);
+};
+
+const getLastName = () => getRandomFromArray(generator.lastNames);
+
+const getBackground = () => getRandomFromArray(generator.backgrounds);
+
+const getTraits = () => {
+  const physique = getRandomFromArray(generator.traits.physique).toLowerCase();
+  const skin = getRandomFromArray(generator.traits.skin).toLowerCase();
+  const hair = getRandomFromArray(generator.traits.hair).toLowerCase();
+  const face = getRandomFromArray(generator.traits.face).toLowerCase();
+
+  const speech = getRandomFromArray(generator.traits.speech).toLowerCase();
+  const clothing = getRandomFromArray(generator.traits.clothing).toLowerCase();
+
+  const virtue = getRandomFromArray(generator.traits.virtue).toLowerCase();
+  const vice = getRandomFromArray(generator.traits.vice).toLowerCase();
+  const reputation = getRandomFromArray(
+    generator.traits.reputation
+  ).toLowerCase();
+
+  const misfortune = getRandomFromArray(
+    generator.traits.misfortunes
+  ).toLowerCase();
+
+  return [
+    `Hai un fisico ${physique}, la pelle ${skin}, ${hair} e la faccia ${face}.`,
+    `Hai una parlata ${speech} e indossi ${clothing}.`,
+    `Sei ${vice}, ma ${virtue} e sei noto come ${reputation}.`,
+    `Hai avuto la sfortuna di essere stato ${misfortune}.`,
+  ].join(" ");
+};
+
+const getArmor = () => {
+  const armor = getRandomFromItemArray(generator.equipment.armor);
+  const helmetAndShield = getRandomFromItemArray(
+    generator.equipment.helmetsAndShields
+  );
+
+  const totalValue = armor.armorValue + helmetAndShield.armorValue;
+
+  return {
+    armor: `${armor.name}, ${helmetAndShield.name}`,
+    armorValue: totalValue >= 3 ? 3 : totalValue,
+  };
+};
+
+const getWeapon = () =>
+  getRandomFromArray(
+    getRandomFromItemArray(generator.equipment.weapons).options
+  );
+
+const getExpeditionaryGear = () =>
+  getRandomFromArray(generator.equipment.expeditionaryGear);
+
+const getTools = () => getRandomFromArray(generator.equipment.tools);
+
+const getTrinkets = () => getRandomFromArray(generator.equipment.trinkets);
+
+const getSpellbooks = () => getRandomFromArray(generator.equipment.spellbooks);
+
+const getBonus = () => {
+  const table = getRandomFromArray(
+    getRandomFromItemArray(generator.equipment.bonus).options
+  );
+
+  if (table === "weapons") return getWeapon();
+  if (table === "tools") return getTools();
+  if (table === "trinkets") return getTrinkets();
+  if (table === "expeditionaryGear") return getExpeditionaryGear();
+  if (table === "armor")
+    return getRandomFromItemArray(generator.equipment.armor);
+  if (table === "spellbooks") return getSpellbooks();
+};
+
+const diceNotation = (string) => {
+  const [count, faces] = string.split("d").map((n) => parseInt(n));
+
+  return Array.from({ length: count }, () => getRandomBetween(1, faces)).reduce(
+    (sum, item) => {
+      sum += item;
+      return sum;
+    },
+    0
+  );
+};
+
+export const generateCharacter = () => {
+  const gearToolsAndTrinkets = `${getExpeditionaryGear()}, ${getTools()} e ${getTrinkets()}`;
+  const gold = diceNotation("3d6");
+  const { armor, armorValue } = getArmor();
+  const bonus = getBonus();
+
+  const totalValue = armorValue + (bonus.armorValue || 0);
+
+  return {
+    name: `${getFirstName()} ${getLastName()}`,
+    background: getBackground().toLowerCase(),
+    traits: getTraits(),
+    age: diceNotation("2d20") + 10,
+
+    hp: diceNotation("1d6"),
+    str: diceNotation("3d6"),
+    dex: diceNotation("3d6"),
+    wil: diceNotation("3d6"),
+    armorValue: totalValue >= 3 ? 3 : totalValue,
+
+    armor,
+    weapon: getWeapon(),
+    gearToolsAndTrinkets,
+    bonus: bonus.name ? bonus.name : bonus,
+    startingSupplies: [
+      "Tre giorni di razioni",
+      "Una torcia",
+      `${gold} pezzi d'oro`,
+    ],
+  };
+};
